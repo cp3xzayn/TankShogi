260059653d6c252cd04b604ddbdfb49d23650f68
h.kato <cp3xzayn@gmail.com>
2021-03-11 17:53:16 +0900

    7×5の盤面を作成し、それぞれのマスにisTriggerのコライダーを設置した。

--- Assets/Scripts/PieceMoveController.cs
+++ Assets/Scripts/PieceMoveController.cs
@@ -5,19 +5,37 @@ using DG.Tweening;
 
 public class PieceMoveController : MonoBehaviour
 {
-    [SerializeField] Transform t;
-    // Start is called before the first frame update
+    [SerializeField] Transform t = null;
+    Vector3 v = new Vector3(1.5f, 0.5f, 1.5f);
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         //1秒で座標（1,1,1）に移動
-        t.DOMove( Vector3.one,  //移動後の座標
+        t.DOMove( v,  //移動後の座標
             1.0f       //時間
             );
     }
+
+    void check(Koma selectkoma)
+    {
+        switch (selectkoma)
+        {
+            case Koma.kinn:
+                break;
+            case Koma.gyoku:
+                break;
+            default:
+                break;
+        }
+    }
+}
+public enum Koma
+{ 
+    kinn,
+    gyoku
 }
